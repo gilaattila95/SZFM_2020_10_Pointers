91f6caed84662fef62e48b25136171003d41ec7d
grestemayster <71188237+grestemayster@users.noreply.github.com>
2020-09-27 21:35:06 +0200

    Create index.php
    index.php

---
+++ src/index.php
@@ -0,0 +1,14 @@
+<?php
+  require_once("action_page.php");
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html" charset="utf-8" />
+<title>SZFM_Fact_Portal</title>
+<link rel="stylesheet" href="style.css">
+</head>
+
+
+</html>
