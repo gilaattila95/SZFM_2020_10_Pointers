43466a9a5fe9cf7012d88d8f435d0edb7561bc48
davidhalasz <38536685+davidhalasz@users.noreply.github.com>
2020-09-27 22:21:08 +0200

    admin eseménykezelés kész

--- src/action_page.php
+++ src/action_page.php
@@ -11,6 +11,38 @@ if (mysqli_connect_errno()) {
     exit();
 }
 
+$uzenet = "";
+
+// Eseménykezelés
+$pEvent = htmlspecialchars(filter_input(INPUT_POST, "event"));
+$gEvent = htmlspecialchars(filter_input(INPUT_GET, "event"));
+$uname = htmlspecialchars(filter_input(INPUT_POST, "uname"));
+$psw = htmlspecialchars(filter_input(INPUT_POST, "psw"));
+$ctext = htmlspecialchars(filter_input(INPUT_POST, "ctext"));
+$kategoria = filter_input(INPUT_POST, "kategoria");
+
+if ($pEvent == "bejelentkezés") {
+    $sql = "select * from admin_userek where uname = '$uname' and aktiv = 1";
+    
+    if($result = mysqli_query($dbc, $sql)) {
+        
+        $row = mysqli_fetch_row($result);
+        if(password_verify($psw, $row[2])) {// Beléphet
+            $_SESSION["auid"] = $row[0];
+            $_SESSION["uname"] = $row[3];
+            if(isset($_SESSION["auid"])) {
+                header("location: administrator.php");
+                exit;
+            }
+        }else { // Nem léphet be
+            $uzenet = "Sikertelen belépés!";
+        }  
+    }
+} else if ($gEvent == "kilepes") {
+        unset($_SESSION["auid"]);
+        unset($_SESSION["uname"]);
+        header("location: index.php");
+    }
 
 
 ?>
