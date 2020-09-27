d3db9ab1c9128ecea68284750deac77588416274
grestemayster <71188237+grestemayster@users.noreply.github.com>
2020-09-27 21:57:47 +0200

    Create sent_cikk.php
    sent_cikk.php alap

---
+++ src/sent_cikk.php
@@ -0,0 +1,23 @@
+<?php
+  require_once("action_page.php");
+?>
+
+<!doctype html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html" charset="utf-8" />
+<title>SZFM_Fact_Portal</title>
+<link rel="stylesheet" href="style.css">
+<style>
+	.adminCikkLista a:link {
+		text-decoration: none;
+	}
+
+	.adminCikkLista .card:hover {
+		background-color: #cfe1f4;
+	}
+</style>
+</head>
+
+
+</html>
