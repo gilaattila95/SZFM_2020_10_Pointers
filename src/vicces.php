591abad9e34ed2e4c51a4960298141695d8b47a5
grestemayster <71188237+grestemayster@users.noreply.github.com>
2020-09-27 21:51:49 +0200

    Update vicces.php
    vicces.php frissítés

--- src/vicces.php
+++ src/vicces.php
@@ -14,7 +14,29 @@
 	Tény Portál
 	</h2>
     <div class="container" align="center">
-      
+    <div class="vertical-center">	
+        <a href="index.php">
+           <button type="button" class="button1">
+             Érdekességek 		
+           </button>
+        </a>
+        <a href="vicces.php">
+            <button type="button" class="button2">
+             Viccesek
+            </button>
+        </a>
+        <a href="cikk.php">
+            <button type="button" class="button1">
+             Cikk beküldése
+            </button>
+        </a>
+        <a href="admin.php">
+            <button type="button" class="button2">
+             Admin belépés
+            </button>
+        </a>
+
+        </div>
     </div>
     <h1 class="version">
     v.0.1
