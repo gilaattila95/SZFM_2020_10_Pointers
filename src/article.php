37dc4a51a8c6804cf728e752e7a7398b170dd968
davidhalasz <38536685+davidhalasz@users.noreply.github.com>
2020-09-27 22:33:18 +0200

    cikk szerkesztőfelület kész

--- src/article.php
+++ src/article.php
@@ -36,7 +36,20 @@
 			</a>  
 		</div>
 
-
+		<div class="container adminCikkLista" align="center" style="margin-top: 10px;">
+			<?php if ($result = $dbc->query($sql)) {
+							$row = $result->fetch_assoc(); ?>
+				<div class="vertical-center">
+					<p class="article" style="font-size: 18px;">
+						<?php print $row['ctext']; ?>                   
+					</p>
+					<form action="" method="post">
+						<input type="submit" name="event" value="aktivalas">
+						<input type="submit" name="event" value="torles">
+					</form>
+				</div>
+			<?php } ?>
+		</div>
 	<h1 class="version">
 		v.0.1
 	</h1>
